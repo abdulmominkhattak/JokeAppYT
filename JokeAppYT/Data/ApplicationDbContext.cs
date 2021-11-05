eb5a5823ee31fe7e4002ca8830c1148f272102ef
Abdul Momin <Abdul Momin@DESKTOP-3SFB3UE>
2021-11-05 22:22:37 +0500

    Add project files.

---
+++ JokeAppYT/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokeAppYT.Models;
+
+namespace JokeAppYT.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<JokeAppYT.Models.Joke> Joke { get; set; }
+	}
+}
---
+++ JokeAppYT/Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeAppYT.Models
+{
+	public class Joke
+	{
+		public int Id { get; set; }
+		public string jokeQuestion { get; set; }
+
+		public string jokeAnswer { get; set; }
+
+		public Joke()
+		{
+
+		}
+	}
+}
